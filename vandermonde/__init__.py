45c6ed7370950a6caa050f2943d69722cc9555cb
Nico Schlömer <nico.schloemer@gmail.com>
2019-05-15 11:23:02 +0200

    add flake8 config

--- vandermonde/__init__.py
+++ vandermonde/__init__.py
@@ -11,3 +11,15 @@ from .__about__ import (
 
 #
 from .main import matrix, det, solve, solve_transpose
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__status__",
+    "__license__",
+    "matrix",
+    "det",
+    "solve",
+    "solve_transpose",
+]
